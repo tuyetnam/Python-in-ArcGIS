b481f281373b674035c74ed08f8ea7e070ddf256
TNT Nguyen <88358162+tuyetnam@users.noreply.github.com>
2022-05-06 11:55:18 +0700

    Add files via upload

---
+++ Make_Netcdf.py
@@ -0,0 +1,17 @@
+﻿# coding: utf-8
+import arcpy
+import numpy
+import pandas
+inNetCDFFile = "E:/PAPER WRITING/TROPOMI & OMI NO2/OMI DATA (xem Disk D)/NC FILEs_Boundary_Vietnam/OMI Tro NO2_Ave_2010.nc"
+variable = "OMNO2d_003_ColumnAmountNO2TropCloudScreened"
+XDimension = "lon"
+YDimension = "lat"
+outRasterLayer = "py_2010"
+bandDimmension = ""
+dimensionValues = ""
+valueSelectionMethod = ""
+cellRegistration = ""
+arcpy.MakeNetCDFRasterLayer_md(inNetCDFFile, variable, XDimension, YDimension,
+                               outRasterLayer, bandDimmension, dimensionValues, 
+                               valueSelectionMethod, cellRegistration)
+# <Result 'py_2010'>
